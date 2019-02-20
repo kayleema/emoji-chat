7c6860989767d5552585dccede9d3c4b764ae027
Kaylee Mann <kmann@pivotal.io>
2019-02-20 15:18:33 +0900

    add emoji picker

--- client/src/Feed.js
+++ client/src/Feed.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import 'emoji-mart/css/emoji-mart.css'
+import {Picker} from 'emoji-mart'
 
 export default class Feed extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            posts : [],
-            message : ''
+            posts: [],
+            message: ''
         }
     }
 
@@ -15,14 +17,15 @@ export default class Feed extends Component {
 
     getPosts() {
         fetch('/api/posts')
-            .then(response =>  response.json())
+            .then(response => response.json())
             .then((data) => (data._embedded.posts))
             .then(posts => this.setState({
-                posts: posts
+                posts: posts.sort((a, b) => (new Date(a.createdDate) > new Date(b.createdDate)) ? -1 : 1)
             }));
     }
 
-    onSubmit() {
+    onSubmit(e) {
+        e.preventDefault();
         const body = JSON.stringify({
             "text": this.state.message,
             "subject": "user1"
@@ -38,23 +41,74 @@ export default class Feed extends Component {
     }
 
     onChangeMessage(event) {
-        this.setState({message: event.target.value});
+        // this.setState({message: event.target.value});
+    }
+
+    formatDate(date) {
+        const s = date;
+        const a = s.split(/[^0-9]/);
+        const d = new Date (a[0],a[1]-1,a[2],a[3],a[4],a[5] );
+        return (
+            d.getFullYear() + '🎍' +
+            (d.getMonth() + 1) + '🌙' +
+            d.getDate() + '☀️' +
+            d.getHours() + '⏰' +
+            d.getMinutes() + '⏲️'
+        );
+    }
+
+    onSelectEmoji(emoji) {
+        console.log(emoji);
+        this.setState({
+            message: this.state.message + emoji.native,
+        });
     }
 
     render() {
         return (
             <div className="feed">
                 <div className="post">
-                    <input
-                        onChange={this.onChangeMessage.bind(this)}
-                        type="text"
-                        value={this.state.message}
+                    <form className="search">
+                        <input
+                            onChange={this.onChangeMessage.bind(this)}
+                            type="text"
+                            className="primary"
+                            value={this.state.message}
+                        />
+                        <button className="primary" type="submit"
+                                onClick={this.onSubmit.bind(this)}>→</button>
+                    </form>
+                    <Picker
+                        onSelect={this.onSelectEmoji.bind(this)}
+                        style={{width: '100%'}}
+                        title='絵文字を選んでください'
+                        emoji='smile_cat'
+                        i18n={{
+                            search: '検索',
+                            notfound: '見つけられませんでした',
+                            skintext: '肌色を選ぶ',
+                            categories: {
+                                search: '検索結果',
+                                recent: 'よく使う',
+                                people: '人',
+                                nature: '動物と自然',
+                                foods: '食べ物と飲み物',
+                                activity: '活動',
+                                places: '旅行と場所',
+                                objects: '物',
+                                symbols: '記号',
+                                flags: '旗',
+                                custom: 'Custom',
+                            }
+                        }}
+                        emojiTooltip={false}
+                        // showPreview={false}
                     />
-                    <button onClick={this.onSubmit.bind(this)}>　➡︎　</button>
                 </div>
                 {this.state.posts.map((post, index) => (
                     <div key={index} className="post">
-                        {post.text}
+                        <p className="postText">{post.text}</p>
+                        <p className='date'>{this.formatDate(post.createdDate)}</p>
                     </div>
                 ))}
             </div>
